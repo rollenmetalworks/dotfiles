408d8718aec79e4894460cc1458a038c73657be8
rollen <rollen_gomes@yahoo.com>
2016-03-06 16:22:26 +0800

    add nvimrc linking

--- setup.sh
+++ setup.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
 # Install Homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # check if there is an existing .vimrc in the home directory
 TARGET_VIMRC=$HOME/.vimrc
+TARGET_NVIMRC=$HOME/.nvimrc
 TARGET_VIM_HOME_FOLDER=$HOME/.vim
 PROJECT_ROOT=`dirname $0`
 PROJECT_VIMRC=$PWD/.vimrc
 
 if [ -a "$TARGET_VIMRC" ] || [ -L "$TARGET_VIMRC" ]; then
 	echo "There is an existing vimrc file at $TARGET_VIMRC"
+	exit;
+fi
+
+if [ -a "$TARGET_NVIMRC" ] || [ -L "$TARGET_NVIMRC" ]; then
+	echo "There is an existing vimrc file at $TARGET_NVIMRC"
 	exit;
 fi
 
@@ -27,5 +33,8 @@ if [ ! -f "$PROJECT_VIMRC" ]; then
 	exit;
 fi
 
-echo "Linking $PROJCET_VIMRC to $TARGET_VIMRC"
+echo "Linking .vimrc $PROJCET_VIMRC to $TARGET_VIMRC"
 ln -s "$PROJECT_VIMRC" "$TARGET_VIMRC"
+
+echo "Linking .nvimrc $PROJCET_VIMRC to $TARGET_NVIMRC"
+ln -s "$PROJECT_VIMRC" "$TARGET_NVIMRC"
