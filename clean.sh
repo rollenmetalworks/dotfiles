0de7d435d9ad1a9ac6f1184499fcf13521942ea4
rollen <rollen_gomes@yahoo.com>
2016-03-06 18:18:57 +0800

    sync files that the clean script removes

--- clean.sh
+++ clean.sh
@@ -1,4 +1,10 @@
-rm -rf ~/.nvim
-rm -rf ~/.vim
-rm ~/.vimrc 
-rm ~/.nvimrc
+TARGET_VIMRC=$HOME/.vimrc
+TARGET_NVIMRC=$HOME/.config/nvim/init.vim
+TARGET_VIM_HOME_FOLDER=$HOME/.vim
+TARGET_NVIM_HOME_FOLDER=$HOME/.nvim
+
+rm $TARGET_VIMRC
+rm $TARGET_NVIMRC
+rm -rf $TARGET_VIM_HOME_FOLDER
+rm -rf $TARGET_NVIM_HOME_FOLDER
+
